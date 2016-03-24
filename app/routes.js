7c86d2a233d4d733d272788d7d545e61cf74138f
Brian Wynn <b_wynn@apple.com>
2016-03-24 11:57:01 -0700

    Moved on to the second portion of the tutorial. Updated routes, query posts added partials and modified the index to show the dynamic content.

--- app/routes.js
+++ app/routes.js
@@ -35,4 +35,61 @@ module.exports = function(app) {
       res.json(req.body);
     });
   });
+
+  // retrieves JSON records for all users who meet a certain set of query conditions
+  app.post('/query/', function(req, res) {
+
+    // grab all of the query parameters from the body
+    var lat = req.body.latitude;
+    var long = req.body.longitude;
+    var distance = req.body.distance;
+    var male = req.body.male;
+    var female = req.body.female;
+    var other = req.body.other;
+    var minAge = req.body.minAge;
+    var maxAge = req.body.maxAge;
+    var favLang = req.body.favlang;
+    var reqVerified = req.body.reqVerified;
+
+    // opens a generic mongoose query. depending on the post body we will...
+    var query = User.find({});
+
+    // ...include filter by Max Distance (converting miles to meters)
+    if (distance) {
+
+      // using mongodb's geospatial querying features.
+      query.where('location').near({center: {type: 'Point', coordinates: [long, lat]},
+        // converting meters to miles. specifying spherical geometry (for globe)
+        maxDistance: distance * 1609.34, spherical: true});
+    }
+
+    if (male || female || other) {
+      query.or([{'gender': male}, {'gender': female}, {'gender': other}]);
+    }
+
+    if (minAge) {
+      query = query.where('age').gte(minAge);
+    }
+
+    if (maxAge) {
+      query = query.where('age').lte(maxAge);
+    }
+
+    if (favLang) {
+      query = query.where('favlang').equals(favLang);
+    }
+
+    if (reqVerified) {
+      query = query.where('htmlverified').equals("Yep (Thanks for giving us real data!)");
+    }
+
+    // execute query and return the query results
+    query.exec(function(err, users) {
+      if (err) {
+        res.send(err);
+      }
+
+      res.json(users);
+    });
+  });
 };
--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,17 @@
-var app = angular.module('meanMapApp', ['AddCtrl', 'geolocation', 'Gservice']);
+var app = angular.module('meanMapApp', ['AddCtrl', 'geolocation', 'Gservice', 'ngRoute'])
+  // Configures angualr routing -- showing the relevant view and controller when needed
+  // consider moving this into it's own module
+  .config(function($routeProvider){
+    // Join Team Control Panel
+    $routeProvider
+      .when('/join', {
+        controller: 'addCtrl',
+        templateUrl: 'partials/addForm.html',
+      })
+      .when('/find', {
+        templateUrl: 'partials/queryForm.html',
+      })
+      .otherwise({
+        redirectTo: '/join'
+      })
+  });
